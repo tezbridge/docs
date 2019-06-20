c12d0418dda18e4719b8f236367806321e2aa22a
catsigma <im@catsigma.com>
2019-06-20 18:04:53 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'TezBridge documentation',
+  description: 'Documentation for TezBridge',
+  head: [
+    ['link', { rel: 'icon', href: 'https://next.tezbridge.com/favicon.90b42df8.png' }]
+  ],
+  themeConfig: {
+    logo: 'https://avatars2.githubusercontent.com/u/33521489?s=200&v=4',
+    nav: [
+      { text: 'Github', link: 'https://github.com/tezbridge' }
+    ]
+  }
+}
