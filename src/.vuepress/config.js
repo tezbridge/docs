fbeea932378a11a6476d755df38a2ffbb00b58f0
catsigma <im@catsigma.com>
2019-06-21 16:55:33 +0800

    update config

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
+  title: 'TezBridge',
   head: [
     ['link', { rel: 'icon', href: 'https://www.tezbridge.com/favicon.90b42df8.png' }]
   ],
   themeConfig: {
-    displayAllHeaders: true,
     logo: 'https://avatars2.githubusercontent.com/u/33521489?s=200&v=4',
     nav: [
       {text: 'Github', link: 'https://github.com/tezbridge'}
