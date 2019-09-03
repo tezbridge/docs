277378ac7e04507230ae7c7ec33d1a40f9eaf3c7
catsigma <im@catsigma.com>
2019-09-03 22:40:27 +0800

    adding first gif; GitLoader component

---
+++ src/.vuepress/components/GifLoader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="is_shown = !is_shown">{{is_shown ? 'Hide' : 'Show'}} screenshot record</button>
+    <img :src="src" v-if="is_shown" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['src'],
+  data() {
+    return {
+      is_shown: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+button { margin-bottom: 8px; transition: all 0.2s; cursor: pointer; border: 2px solid #3e8daf; padding: 4px 8px; border-radius: 4px; background: #e8eff7; color: #3e6baf; outline: none;}
+button:hover { color: white; background: #3e7baf }
+</style>
--- src/user.md
+++ src/user.md
@@ -1,6 +1,16 @@
-# How to list
+# How to
 
 ## Creating new key
+### By mnemonic words
+At the index page of TezBridge, go **Create key -> Mnemonic**.
+
+<gif-loader src="/imgs/create_key_mnemonic.gif" />
+
+### By Ed25519
+
+### By Secp256k1
+
+### By NIST P256
 
 ## Importing secret key
 
