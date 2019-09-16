d25c02949256611a4d1c048e51816724a061ca70
catsigma <im@catsigma.com>
2019-09-17 03:26:48 +0800

    update features in user.md

--- src/.vuepress/components/GifLoader.vue
+++ src/.vuepress/components/GifLoader.vue
@@ -62,7 +62,7 @@ export default {
 </script>
 
 <style scoped>
-button { margin-bottom: 8px; transition: all 0.2s; cursor: pointer; border: 2px solid #3e8daf; padding: 4px 8px; border-radius: 4px; background: #e8eff7; color: #3e6baf; outline: none;}
-button:hover { color: white; background: #3e7baf }
+button { margin-bottom: 8px; transition: all 0.2s; cursor: pointer; border: 2px solid #85b5ca; padding: 4px 8px; border-radius: 4px; background: #e8eff7; color: #496998; outline: none;}
+button:hover { color: white; background: #85b5ca }
 img {border: 1px dotted #3e7baf;}
 </style>
